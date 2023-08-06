847ace14ad2d85eb1518e8fc88de9527c3b42404
Dewanshh <73938082+Dewanshh@users.noreply.github.com>
2023-08-06 16:19:28 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const https = require("https");
+const request = require("request");
+
+const path = require("path")
+const app = express();
+
+app.set('views', path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+    res.render('search');
+})
+app.get("/result", function (req, resp) {
+    let query=req.query.search;
+    let url="https://api.themoviedb.org/3/search/movie?api_key=aab21e43e6b08171f5d488bcbae9de95&query="+query;
+    request(url,(error,res,body)=>
+    {
+        if(error){
+            console.log(error);
+        }
+        else{
+            let data=JSON.parse(body);
+            resp.render('result',{data:data,querySearch:query});
+            console.log(data);
+        }
+    })
+    
+    // res.render('result');
+})
+
+
+app.listen(process.env.PORT||3000, function (req, res) {
+    console.log("Running Server 3000");
+})
